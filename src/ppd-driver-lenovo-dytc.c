ebf9e5b7c807b1a5fc799fceef5eb914c4ce535f
Bastien Nocera <hadess@hadess.net>
2020-08-06 15:47:39 +0200

    lenovo-dytc: Implement perfmode
    Implement dytc_perfmode, as per https://patchwork.kernel.org/patch/11678983/

--- src/ppd-driver-lenovo-dytc.c
+++ src/ppd-driver-lenovo-dytc.c
@@ -8,10 +8,13 @@
  */
 
 #include <gudev/gudev.h>
+#include <gio/gio.h>
 
 #include "ppd-driver-lenovo-dytc.h"
+#include "ppd-utils.h"
 
 #define LAPMODE_SYSFS_NAME "dytc_lapmode"
+#define PERFMODE_SYSFS_NAME "dytc_perfmode"
 
 struct _PpdDriverLenovoDytc
 {
@@ -57,11 +60,25 @@ sysfs_attr_as_boolean (GUdevDevice *device,
   return (g_strcmp0 (contents, "1") == 0);
 }
 
+static const char *
+profile_to_perfmode_value (PpdProfile profile)
+{
+  switch (profile) {
+  case PPD_PROFILE_POWER_SAVER:
+    return "L";
+  case PPD_PROFILE_BALANCED:
+    return "M";
+  case PPD_PROFILE_PERFORMANCE:
+    return "H";
+  }
+
+  g_assert_not_reached ();
+}
+
 static void
 update_dytc_state (PpdDriverLenovoDytc *dytc)
 {
   gboolean new_lapmode;
-
 
   new_lapmode = sysfs_attr_as_boolean (dytc->device, LAPMODE_SYSFS_NAME);
   if (new_lapmode != dytc->lapmode) {
@@ -94,6 +111,31 @@ uevent_cb (GUdevClient *client,
 }
 
 static gboolean
+ppd_driver_lenovo_dytc_activate_profile (PpdDriver   *driver,
+                                         PpdProfile   profile,
+                                         GError     **error)
+{
+  PpdDriverLenovoDytc *dytc = PPD_DRIVER_LENOVO_DYTC (driver);
+
+  g_return_val_if_fail (dytc->client, FALSE);
+
+  if (profile == PPD_PROFILE_PERFORMANCE &&
+      dytc->lapmode) {
+    g_debug ("Can't switch to performance mode, lapmode is detected");
+    g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED, "Mode is inhibited");
+    return FALSE;
+  }
+
+  if (!ppd_utils_write_sysfs (dytc->device, PERFMODE_SYSFS_NAME, profile_to_perfmode_value (profile), error)) {
+    g_debug ("Failed to write to perfmode: %s", (* error)->message);
+    return FALSE;
+  }
+
+  g_debug ("Successfully switched to profile %s", ppd_profile_to_str (profile));
+  return TRUE;
+}
+
+static gboolean
 ppd_driver_lenovo_dytc_probe (PpdDriver *driver)
 {
   const gchar * const subsystem[] = { "platform", NULL };
@@ -114,7 +156,8 @@ ppd_driver_lenovo_dytc_probe (PpdDriver *driver)
     if (g_strcmp0 (g_udev_device_get_name (dev), "thinkpad_acpi") != 0)
       continue;
 
-    if (!g_udev_device_get_sysfs_attr (dev, LAPMODE_SYSFS_NAME))
+    if (!g_udev_device_get_sysfs_attr (dev, LAPMODE_SYSFS_NAME) ||
+        !g_udev_device_get_sysfs_attr (dev, PERFMODE_SYSFS_NAME))
       break;
 
     dytc->device = g_object_ref (dev);
@@ -159,6 +202,7 @@ ppd_driver_lenovo_dytc_class_init (PpdDriverLenovoDytcClass *klass)
 
   driver_class = PPD_DRIVER_CLASS(klass);
   driver_class->probe = ppd_driver_lenovo_dytc_probe;
+  driver_class->activate_profile = ppd_driver_lenovo_dytc_activate_profile;
 }
 
 static void
