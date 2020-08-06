26af80e115d2143186f8f968d7ddee127da667d5
Bastien Nocera <hadess@hadess.net>
2020-08-06 12:34:51 +0200

    lenovo-dytc: Some reindentation

--- src/ppd-driver-lenovo-dytc.c
+++ src/ppd-driver-lenovo-dytc.c
@@ -26,14 +26,14 @@ G_DEFINE_TYPE (PpdDriverLenovoDytc, ppd_driver_lenovo_dytc, PPD_TYPE_DRIVER)
 
 static GObject*
 ppd_driver_lenovo_dytc_constructor (GType                  type,
-                                            guint                  n_construct_params,
-                                            GObjectConstructParam *construct_params)
+                                    guint                  n_construct_params,
+                                    GObjectConstructParam *construct_params)
 {
   GObject *object;
 
   object = G_OBJECT_CLASS (ppd_driver_lenovo_dytc_parent_class)->constructor (type,
-                                                                                n_construct_params,
-                                                                                construct_params);
+                                                                              n_construct_params,
+                                                                              construct_params);
   g_object_set (object,
                 "driver-name", "lenovo_dytc",
                 "profile", PPD_PROFILE_PERFORMANCE,
