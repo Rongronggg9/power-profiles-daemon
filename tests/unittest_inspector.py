f742b017b1e6aa5836551ec7c1e3ea59064c2706
Mario Limonciello <mario.limonciello@amd.com>
2024-02-05 15:24:33 -0600

    Reformat unittest_inspector.py using pylint and black

--- tests/unittest_inspector.py
+++ tests/unittest_inspector.py
@@ -22,23 +22,25 @@ import inspect
 import os
 import unittest
 
+
 def list_tests(module):
     tests = []
     for name, obj in inspect.getmembers(module):
         if inspect.isclass(obj) and issubclass(obj, unittest.TestCase):
             cases = unittest.defaultTestLoader.getTestCaseNames(obj)
-            tests += [ (obj, '{}.{}'.format(name, t)) for t in cases ]
+            tests += [(obj, "{}.{}".format(name, t)) for t in cases]
     return tests
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('unittest_source', type=argparse.FileType('r'))
+    parser.add_argument("unittest_source", type=argparse.FileType("r"))
 
     args = parser.parse_args()
     source_path = args.unittest_source.name
     spec = importlib.util.spec_from_file_location(
-        os.path.basename(source_path), source_path)
+        os.path.basename(source_path), source_path
+    )
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
 
